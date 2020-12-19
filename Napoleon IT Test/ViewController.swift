ccc0d3e68fc57dfc7689bd1309250b0b56fe1730
Kavars <kavars@yandex.ru>
2020-12-19 11:08:28 +0300

    Initial Commit

---
+++ Napoleon IT Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Napoleon IT Test
+//
+//  Created by Kirill Varshamov on 19.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
